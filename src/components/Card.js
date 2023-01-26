61fe2ae1e98b3d83c387afa05898bf7578a13b25
mienblack <damienfelipe2015@gmail.com>
2023-01-25 23:33:48 -0300

    homepage

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,7 @@
+import styles from "@/styles/Card.module.css"
+
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Card({ pokemon }) {
     let pokeIndex = ""
@@ -12,14 +15,18 @@ export default function Card({ pokemon }) {
     }
 
     return (
-        <div>
+        <div className={styles.pokeCard}>
             <Image
                 src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokeIndex}.png`}
                 width={120}
                 height={120}
                 alt={pokemon.name}
             />
+            <p>#{pokemon.id}</p>
             <h3>{pokemon.name}</h3>
+            <Link
+                href={`/pokemon/${pokemon.id}`}
+            >Detalhes</Link>
         </div>
     );
 }
